b8eb5cf06cbfcc58231081cb0b545295777b92aa
Akshit Bariya <adb02bariya@gmail.com>
2023-01-21 02:41:17 +0530

    First

---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+function Card() {
+  return(    
+    <div className='card'>
+      <h2>Total</h2>
+      <h1>x</h1>
+      <h1>India</h1>
+    </div>
+  );
+}
+
+export default Card;
