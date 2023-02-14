e74ac4a994a3b963a68e8a54833af8067d07dc47
Akshit Bariya <adb02bariya@gmail.com>
2023-02-14 14:01:48 +0530

    second

--- src/Components/Covid.jsx
+++ src/Components/Covid.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect,useState } from 'react';
-import Nature from "./images/covid-cells.jpg";
+// import Nature from "./images/covid-cells.jpg";
 // import Card from './Card';
 // import {Grid} from '@mui/material';
 import './Covid.css';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import './Card.css';
 // import { Grid } from '@mui/material';
 // import Grid from '@mui/material/Grid';
@@ -11,15 +12,17 @@ import './Card.css';
 function Covid() {
 
     const [data, setData] = useState([]);
-
+    function refreshPage() {
+        window.location.reload(true);
+      }
     const getCovidData = async () => {
         try {
             const res = await fetch('	https://data.covid19india.org/data.json');
             const actualData = await res.json();
-            console.log(actualData.statewise[0]);
+            // console.log(actualData.statewise[0]);
             setData(actualData.statewise[0]);
         } catch (err) {
-            console.log(err);
+            // console.log(err);
         }
     };
 
@@ -28,56 +31,50 @@ function Covid() {
     }, []);
     return (
         <>
-            <div className='container'>
-                <div className='card_top'>
-                    <span className='top-bar'>🔴LIVE</span>
-                    <span className='bottom-bar'>COVID-19 TRACKER</span>
+        <div className='covid-main'>
+            <div className='c-container'>
+            <div className='card_top'>
+                    <span className='bottom-bar'>COVID-19 CASES TRACKER</span>
+                    <RefreshIcon onClick={refreshPage} className="refresh"/> 
                 </div>
-                <div className='card_main' >
-                {/* <img src={Nature} alt="bg" /> */}
-
-                    <div className='card'>
-                        <h2>Our</h2>
-                        <h1>Covid-19 Data</h1>
-                        <h1>India</h1>
-                    </div>
-
-                    <div className='card'>
-                        <h2>Total</h2>
-                        <h1>Active</h1>
-                        <h1>{data.active}</h1>
-
-                    </div>
-
-                    <div className='card'>
-                        <h1>Total</h1>
-                        <h2>Confirmed</h2>
-                        <h1>{data.confirmed}</h1>
-
-                    </div>
-
-                    <div className='card'>
-                        <h2>Total</h2>
-                        <h1>Deaths</h1>
-                        <h1>{data.deaths}</h1>
-
-                    </div>
-
-                    <div className='card'>
-                        <h2>Total </h2>
-                        <h1>Recovered</h1>
-                        <h1>{data.recovered}</h1>
-
-                    </div>
-
-                    <div className='card' >
-                        <h2>Last Updated</h2>
-                        <h1>{data.lastupdatedtime}</h1>
-                        {/* <h1>India</h1> */}
-                    </div>
-                </div>
-
+                <div className='c-main'>
+            <span className='data'> Active</span>
+            <span className='data'> {data.active}</span>
             </div>
+            <div className='c-main'>
+            <span className='data'>Confirmed</span>
+            <span className='data'>{data.confirmed}</span>
+            </div>
+            <div className='c-main'>
+            <span className='data'>  Deaths</span>
+            <span className='data'> {data.deaths}</span>
+            </div>
+            <div className='c-main'>
+            <span className='data'>  Recovered</span>
+            <span className='data'> {data.recovered}</span>
+            </div>
+            <div className='c-main'>
+            <span className='data'>  Delta confirmed</span>
+            <span className='data'> {data.deltaconfirmed}</span>
+            </div>
+            <div className='c-main'>
+            <span className='data'>  Delta deaths</span>
+            <span className='data'> {data.deltadeaths}</span>
+            </div>
+            <div className='c-main'>
+            <span className='data'>  Delta recovered</span>
+            <span className='data'> {data.deltarecovered}</span>
+            </div>
+            <div className='c-bottom'>
+            <span className='data'> Last updated time</span>
+            <span className='data'>{data.lastupdatedtime}</span>
+            </div>
+            
+            </div>
+        </div>
+
+
+            
 
         </>
     );
